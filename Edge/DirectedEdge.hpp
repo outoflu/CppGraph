225496652e37df439ced61d0351e68a2bd6a0387
outoflu <126671417+outoflu@users.noreply.github.com>
2024-05-15 06:42:37 +0000

    play int codespace

---
+++ Edge/DirectedEdge.hpp
@@ -0,0 +1,6 @@
+#ifndef _DIRECTED_EDGE_H
+#define _DIRECTED_EDGE_H
+
+#include "Edge.hpp"
+
+#endif
---
+++ Edge/Edge.hpp
@@ -0,0 +1,10 @@
+#ifndef _EDGE_HPP
+#define _EDGE_HPP
+
+#include <memory>
+#include <optional>
+#include <utility>
+
+
+
+#endif 
